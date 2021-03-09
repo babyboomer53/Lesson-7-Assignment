642f3cc575d93532d5255ddb1c79ef663ab06bd3
Edgar Cole <edgarcolejr@gmail.com>
2021-03-08 19:47:08 -0800

    It would be prudent to take a checkpoint here.

---
+++ Lessons.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS Lessons (Lesson_Num INTEGER, Title VARCHAR(30));
+INSERT IGNORE INTO Lessons VALUES (1, 'Stream Input/Output');
+INSERT IGNORE INTO Lessons VALUES (2, 'Regular Expressions');
+INSERT IGNORE INTO Lessons VALUES (3, 'Streams');
+INSERT IGNORE INTO Lessons VALUES (4, 'XML');
+INSERT IGNORE INTO Lessons VALUES (5, 'Concurrent Programming');
+INSERT IGNORE INTO Lessons VALUES (6, 'Networking');
+INSERT IGNORE INTO Lessons VALUES (7, 'Database');
+INSERT IGNORE INTO Lessons VALUES (8, 'Modules');
+INSERT IGNORE INTO Lessons VALUES (9, 'Scripting and Compiling');
+
+
--- src/EDU/UCSD/Extension/Lesson7Database.java
+++ src/EDU/UCSD/Extension/Lesson7Database.java
@@ -1,11 +1,27 @@
 package EDU.UCSD.Extension;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lesson7Database {
+
     private String filename = null;
-    private final String databaseName = "COREJAVA";
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/corejava";
+
+    // Database credentials
+    static final String USERNAME = "babyboomer";
+    static final String PASSWORD = "Tl8L2^7GwIoo";
+
+    Scanner scanner = null;
+
+    Connection connection = null;
+    Statement statement = null;
 
     public Lesson7Database(String filename) {
         this.filename = filename;
@@ -23,10 +39,6 @@ public class Lesson7Database {
         }
     }
 
-    public String getDatabaseName() {
-        return databaseName;
-    }
-
     public static void syntaxSummary() {
         var commandName = "Lesson7Database";
         System.out.printf("%n%-7s%-16s%-11s%s%n%-7s%-16s%-11s%s%n",
@@ -37,7 +49,7 @@ public class Lesson7Database {
                 "",
                 commandName,
                 "<filename>",
-                "# Listen for connections on port <number>");
+                "# Process the SQL statements in <filename>");
     }
 
     public static boolean theFileNameIsValid(String filename) {
@@ -51,31 +63,75 @@ public class Lesson7Database {
         }
     }
 
-    public boolean startTheDatabase(String filename) {
+    public boolean connectToTheDatabase() throws SQLException {
+        try {
+            //STEP 2: Register JDBC driver
+            // N O P E !
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            return false;
+        }
         return true;
     }
 
-    public boolean connectToTheDatabase(String filename) {
-        return true;
+    public boolean populateTheTable() throws IOException, SQLException {
+        System.out.println("Populating the table…");
+        scanner = new Scanner(Paths.get(filename), StandardCharsets.UTF_8);
+        statement = connection.createStatement();
+        while (true) {
+            if (!scanner.hasNextLine()) return false;
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty()) return true;
+            if (line.endsWith(";")) // remove trailing semicolon
+                line = line.substring(0, line.length() - 1);
+            try {
+                if (statement.execute(line)) {
+                    try (ResultSet resultSet = statement.getResultSet()) {
+                        continue;
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                } else {
+                    int updateCount = statement.getUpdateCount();
+                }
+            } catch (SQLException sqlException) {
+                for (Throwable throwable : sqlException)
+                    throwable.printStackTrace();
+            }
+        }
     }
 
-    public boolean populateTheTable(String tableName) {
-        return true;
-    }
+    public void dumpTheDatabase() throws SQLException {
+        System.out.println("Dumping the table…\n");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM Lessons");
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
 
-    public boolean dumpTheDatabase(String tableName) {
-        return true;
+        for (int index = 1; index <= columnCount; index++) {
+            System.out.printf("%-12s",metaData.getColumnLabel(index));
+        }
+
+        System.out.printf("%n%s%n","-".repeat(40));
+
+        while (resultSet.next()) {
+            for (int index = 1; index <= columnCount; index++) {
+                System.out.printf("%-12s",resultSet.getString(index));
+            }
+            System.out.println();
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("%nInstance of:   %s%nDatabase name: %s%nFilename:      %s%n",
+        return String.format("%nInstance of:   %s%nFilename:      %s%n",
                 this.getClass().getSimpleName(),
-                this.getDatabaseName(),
                 this.getFilename());
     }
 
-    public static void main(String[] arguments) {
+    public static void main(String[] arguments) throws SQLException, IOException {
         Lesson7Database lesson7Database;
         try {
             var argument = arguments[0];
@@ -94,7 +150,9 @@ public class Lesson7Database {
                     if (theFileNameIsValid(arguments[index])) {
                         lesson7Database = new Lesson7Database(arguments[index]);
                         System.out.println(lesson7Database);
-                        lesson7Database.setFilename("Edgar//Cole");
+                        lesson7Database.connectToTheDatabase();
+                        lesson7Database.populateTheTable();
+                        lesson7Database.dumpTheDatabase();
                     } else {
                         System.err.printf("%n\"%s\" is not a valid file name!%n", arguments[index]);
                         Lesson7Database.syntaxSummary();
